5e6653fcda776e5a1a2ae4476ff9c9b4c62680bc
DennnE <denner_erthal@hotmail.com>
2017-05-24 11:33:29 -0300

    Add files via upload

--- builder.js
+++ builder.js
@@ -17,58 +17,60 @@ var ELECTRON_PACKAGE_MAC_DEFAULTS = Object.assign({}, ELECTRON_PACKAGER_DEFAULTS
     arch: 'x64',
 });
 
-packager(Object.assign({}, ELECTRON_PACKAGE_MAC_DEFAULTS, {
-    platform: 'darwin'
-}), function done_callback(err, appPaths) {
-    if (err) throw err;
-    console.log('Done creating Darwin.')
-});
-
-builder.build({
-    targets: Platform.LINUX.createTarget(),
-    projectDir: process.cwd(),
-    config: {
-        "electronVersion": ELECTRON_VERSION,
-        "linux": {
-            "target": "AppImage",
-            "executableName": "electron-sample",
-            "desktop": {
-                "Type": "Application",
-                "Name": "Electron Sample"
+if (process.env['TRAVIS_OS_NAME'] === 'linux') {
+    builder.build({
+        targets: Platform.LINUX.createTarget(),
+        projectDir: process.cwd(),
+        config: {
+            "electronVersion": ELECTRON_VERSION,
+            "linux": {
+                "target": "snap",
+                "executableName": "electron-sample",
+                "desktop": {
+                    "Type": "Application",
+                    "Name": "Electron Sample",
+                    "Exec": "electron-sample %U"
+                }
+            },
+            "directories": {
+                "output": process.cwd()
             }
-        },
-        "directories": {
-            "output": process.cwd() + '/dist'
         }
-    }
-}).then(() => {
-    console.log('Done creating AppImage.')
-})
-.catch((error) => {
-    console.log(error);
-});
+    }).then(() => {
+        console.log('Done creating Snap.')
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+} else {
+    packager(Object.assign({}, ELECTRON_PACKAGE_MAC_DEFAULTS, {
+        platform: 'darwin'
+    }), function done_callback(err, appPaths) {
+        if (err) throw err;
+        console.log('Done creating Darwin.')
+    });
 
-builder.build({
-    targets: Platform.LINUX.createTarget(),
-    projectDir: process.cwd(),
-    config: {
-        "electronVersion": ELECTRON_VERSION,
-        "linux": {
-            "target": "snap",
-            "executableName": "electron-sample",
-            "desktop": {
-                "Type": "Application",
-                "Name": "Electron Sample",
-                "Exec": "electron-sample %U"
+    builder.build({
+        targets: Platform.LINUX.createTarget(),
+        projectDir: process.cwd(),
+        config: {
+            "electronVersion": ELECTRON_VERSION,
+            "linux": {
+                "target": "AppImage",
+                "executableName": "electron-sample",
+                "desktop": {
+                    "Type": "Application",
+                    "Name": "Electron Sample"
+                }
+            },
+            "directories": {
+                "output": process.cwd() + '/dist'
             }
-        },
-        "directories": {
-            "output": process.cwd()
         }
-    }
-}).then(() => {
-    console.log('Done creating Snap.')
-})
-.catch((error) => {
-    console.log(error);
-});
+    }).then(() => {
+        console.log('Done creating AppImage.')
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
