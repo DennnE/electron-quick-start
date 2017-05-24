1a2f732d0e571cdd0197ef0f6193afcc386ba21e
DennnE <denner_erthal@hotmail.com>
2017-05-24 17:59:24 -0300

    Add files via upload

--- builder.js
+++ builder.js
@@ -18,9 +18,35 @@ var ELECTRON_PACKAGE_MAC_DEFAULTS = Object.assign({}, ELECTRON_PACKAGER_DEFAULTS
     arch: 'x64',
 });
 
-packager(Object.assign({}, ELECTRON_PACKAGE_MAC_DEFAULTS, {
-    platform: 'linux'
-}), function done_callback(err, appPaths) {
-    if (err) throw err;
-    child_process.execSync('snapcraft');
-});
+// packager(Object.assign({}, ELECTRON_PACKAGE_MAC_DEFAULTS, {
+//     platform: 'linux'
+// }), function done_callback(err, appPaths) {
+//     if (err) throw err;
+//     child_process.execSync('snapcraft', {
+//         cwd: '/snap'
+//     });
+// });
+    builder.build({
+        targets: Platform.LINUX.createTarget(),
+        projectDir: process.cwd(),
+        config: {
+            "electronVersion": ELECTRON_VERSION,
+            "linux": {
+                "target": "snap",
+                "executableName": "electron-sample",
+                "desktop": {
+                    "Type": "Application",
+                    "Name": "Electron Sample",
+                    "Exec": "electron-sample"
+                }
+            },
+            "directories": {
+                "output": process.cwd()
+            }
+        }
+    }).then(() => {
+        console.log('Done creating Snap.')
+    })
+    .catch((error) => {
+        console.log(error);
+    });
