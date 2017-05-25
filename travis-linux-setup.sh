8722f63664208b34a2a00997e8368c19befb9597
DennnE <denner_erthal@hotmail.com>
2017-05-25 12:05:43 -0300

    Update travis-linux-setup.sh

--- travis-linux-setup.sh
+++ travis-linux-setup.sh
@@ -2,18 +2,13 @@
 
 set -xe
 
-TRAVIS_BUILD_STEP="$1"
 DOCKER_BUILDER_NAME='builder'
 THIS_PATH=$(dirname $0)
 
-if [ "$TRAVIS_BUILD_STEP" == "before_install" ]; then
-    if [ -n "$ARCH" ]; then DOCKER_IMAGE="$ARCH/$DOCKER_IMAGE"; fi
+if [ -n "$ARCH" ]; then DOCKER_IMAGE="$ARCH/$DOCKER_IMAGE"; fi
 
-    docker run --name $DOCKER_BUILDER_NAME -e LANG=C.UTF-8 -e TERM \
-           -v $PWD:$PWD -w $PWD/$THIS_PATH -td $DOCKER_IMAGE
+docker run --name $DOCKER_BUILDER_NAME -e LANG=C.UTF-8 -e TERM \
+       -v $PWD:$PWD -w $PWD/$THIS_PATH -td $DOCKER_IMAGE
 
-    docker exec -i $DOCKER_BUILDER_NAME apt-get update -q
-    docker exec -i $DOCKER_BUILDER_NAME apt-get install -y snapcraft
-elif [ "$TRAVIS_BUILD_STEP" == "script" ]; then
-    docker exec -i $DOCKER_BUILDER_NAME snapcraft
-fi
+docker exec -i $DOCKER_BUILDER_NAME apt-get update -q
+docker exec -i $DOCKER_BUILDER_NAME apt-get install -y snapcraft
