39c23103df9c158689298c128ac806ad47d3b775
DennnE <denner_erthal@hotmail.com>
2017-05-25 10:51:58 -0300

    Update travis-linux-setup.sh

--- travis-linux-setup.sh
+++ travis-linux-setup.sh
@@ -23,8 +23,6 @@ if [ "$TRAVIS_BUILD_STEP" == "before_install" ]; then
 
     docker_exec apt-get update -q
     docker_exec apt-get install -y snapcraft
-    sudo apt-get install -y nodejs
-    sudo apt-get install -y npm
 elif [ "$TRAVIS_BUILD_STEP" == "script" ]; then
     docker_exec snapcraft
 fi
