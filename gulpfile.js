38c6ae7772ce76fd711e0a3bfee82b9a128521e9
DennnE <denner_erthal@hotmail.com>
2017-05-25 12:18:10 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,6 @@ require('./gulp/travis');
 gulp.task('package-linux', function(cb) {
     runSequence(
         'package-electron-base',
-        function () {
-            child_process.exec('docker exec -i builder snapcraft', function (err, stdout, stderr) {
-                if (err) throw err;
-                es.merge(
-                    gulp
-                        .src('*.snap')
-                        .pipe(gulp.dest('dist/_downloads/linux'))
-                )
-            });
-        }
+        cb
     );
 });
